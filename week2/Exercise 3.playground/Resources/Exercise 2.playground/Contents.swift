92205b8a21f1c039698fdc1535d041188fc61df5
Lin Han Sheng <linhansheng0506@e.neivce.edu.my>
2024-11-13 11:17:05 +0800

    done 1,2,3,4,5

---
+++ week2/Exercise 3.playground/Resources/Exercise 2.playground/Contents.swift
@@ -0,0 +1,4 @@
+import UIKit
+
+var name: String = "James", age: Int = 12, price: Double = 9.8, lose: Bool = false
+print("\(name) = \(type(of:name))\n\(age) = \(type(of:age))\n\(price) = \(type(of:price))\n\(lose) = \(type(of:lose))")
