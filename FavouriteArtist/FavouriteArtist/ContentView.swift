b8366d5e0819069c0da00128b8459c21e8061515
Lin Han Sheng <linhansheng0506@e.neivce.edu.my>
2024-12-18 11:21:09 +0800

    week 7

--- FavouriteArtist/FavouriteArtist/ContentView.swift
+++ FavouriteArtist/FavouriteArtist/ContentView.swift
@@ -10,12 +10,94 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack {
+                VStack {
+                    HStack {
+                        Spacer()
+                        Image(systemName: "bell.badge")
+                            .font(.title)
+                            .foregroundColor(.red)
+                        Image(systemName: "person")
+                            .font(.title)
+                            .foregroundColor(.blue)
+                            .padding()
+                    }
+                    HStack {
+                        Text("\tStore")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        Spacer()
+                    }
+                }
+                
+            }
+            ZStack {
+                Color("Color")
+                    .cornerRadius(20)
+                HStack {
+                    VStack {
+                        Text("\n\tG.E.M")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+                    }
+                    Spacer()
+                    Image("GEM")
+                        .resizable()
+                        .aspectRatio(contentMode : .fit)
+                        .padding()
+                }
+            }
+            ZStack {
+                Color("Color2")
+                    .cornerRadius(20)
+                HStack {
+                    Text("\n\tNayeon")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding()
+                    Spacer()
+                    Image("nayeon")
+                        .resizable()
+                        .aspectRatio(contentMode : .fit)
+                        .padding()
+                }
+            }
+            ZStack {
+                Color("Color3")
+                    .cornerRadius(20)
+                HStack {
+                    Text("\n\tChaeyoung")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding()
+                    Spacer()
+                    Image("chaeyoung")
+                        .resizable()
+                        .aspectRatio(contentMode : .fit)
+                        .padding()
+                }
+            }
+            ZStack {
+                Color("Color4")
+                    .cornerRadius(20)
+                HStack {
+                    Text("\n\tSoo Jin")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding()
+                    Spacer()
+                    Image("soojin")
+                        .resizable()
+                        .aspectRatio(contentMode : .fit)
+                        .padding()
+                }
+            }
         }
-        .padding()
     }
 }
 
