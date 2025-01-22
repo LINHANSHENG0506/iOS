a0c4f42d97ab1c04521c28e54eff76104e62b64e
Lin Han Sheng <linhansheng0506@e.neivce.edu.my>
2025-01-22 11:49:01 +0800

    week 11 done

---
+++ week11/searchOption/searchOption/Day Time Weather.swift
@@ -0,0 +1,18 @@
+//
+//  Day Time Weather.swift
+//  searchOption
+//
+//  Created by labuser on 22/01/2025.
+//
+
+import SwiftUI
+
+struct Day_Time_Weather: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Day_Time_Weather()
+}
---
+++ week11/searchOption/searchOption/weatherView.swift
@@ -0,0 +1,18 @@
+//
+//  weatherView.swift
+//  searchOption
+//
+//  Created by labuser on 22/01/2025.
+//
+
+import SwiftUI
+
+struct weatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    weatherView()
+}
